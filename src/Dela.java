de9de1e7c587678636478461499e74dad7cdc68d
sqwuilo <serezhaonufrienko@yandex.ru>
2023-01-13 12:35:32 +0300

    Initial commit

---
+++ src/Dela.java
@@ -0,0 +1,42 @@
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class Dela {
+
+    ArrayList<String> onToday = new ArrayList<>();
+    void Delaa(int hour, int minutes){
+        Date date1 = new Date();
+        LocalTime time = LocalTime.of(hour, minutes, 0, 0);
+
+        int hours = date1.getHours();
+        int hoursDela = time.getHour();
+
+        int minute = date1.getMinutes();
+        int minuteDela = time.getMinute();
+
+        if (hoursDela < hours){
+            System.out.println("Ваши дела не могут быть записаны на прошедшее время");
+        } else if (hoursDela == hours) {
+            if (minuteDela <= minute){
+                System.out.println("Похоже вы записали дела на прошедшее время, попробуйте указать верное число");
+            }else {
+                System.out.println("Прекрасно, ваши дела записаны на: " + time);
+            }
+        } else {
+            System.out.println("Прекрасно, ваши дела записаны на: " + time);
+            onToday.add(String.valueOf(time));
+        }
+
+    }
+
+    void DelaTodayList(){
+        System.out.println("Ваши дела на сегодня: ");
+        for (String s : onToday) {
+            System.out.println(s);
+        }
+    }
+
+}
